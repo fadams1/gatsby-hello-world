eee3a46ecd3bb3ed21563b1edce664bd66cf71dc
fay <fay737731@yahoo.co.uk>
2019-05-29 18:05:44 +0100

    playing with layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
 
 export default () => (
-    <div style={{color: `purple`}}>
-        <Link to="/contact/">Contact</Link>
-        <Header headerText="Hello Nic!" />
-        <p>Hello Fay!</p>
-        <img src="https://source.unsplash.com/random/400x200" alt=""/>
-    </div>
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import fairyGateTheme from "typography"
+
+const typography = new Typography(fairyGateTheme)
+
+export const { scale, rythm, options } = typography
+export default typography
