746e8a39bd8a95d23ba2769f013a823c19f459ac
fay <fay737731@yahoo.co.uk>
2019-05-29 16:15:54 +0100

    first hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{color: `purple`}}>
+        <h1>Hello Nic!</h1>
+        <p>Hello Fay!</p>
+        <img src="https://source.unsplash.com/random/400x200" alt=""/>
+    </div>
+)
